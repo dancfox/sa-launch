9041300bcee9ff352737e6b022e02b90801568ec
Dan Fox <dfxamzn@amazon.com>
2021-04-08 15:18:19 -0600

    configured cluster of 25

--- artillery/artillery-run-cluster-25.sh
+++ artillery/artillery-run-cluster-25.sh
@@ -5,5 +5,5 @@ artillery run-test  \
     --cluster artillery-cluster \
     --region us-east-1 \
     --count 25 \
-    --launch-config '{"ulimits":[{"name":"nofile","softLimit":"16384","hardLimit":"16384"}]}' \
+    --launch-config '{"ulimits":[{"name":"nofile","softLimit":"32768","hardLimit":"32768"}]}' \
     --launch-type ecs:fargate config.yaml
